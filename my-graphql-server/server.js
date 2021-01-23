3a80f085bb10354e16dc8067b13006ee87677be2
Hugo <hleote@gmail.com>
2021-01-23 21:55:35 +0100

    Extend Apollo server

--- my-graphql-server/server.js
+++ my-graphql-server/server.js
@@ -1,58 +1,67 @@
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-import cors from 'cors';
-import fetch from 'node-fetch';
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import cors from "cors";
+import fetch from "node-fetch";
 
-const baseURL = 'http://localhost:11000';
+const baseURL = "http://localhost:11000";
 
 const schema = gql`
   type Query {
     person: Person
+    facility: Facility
+    exposure: Exposure
   }
- 
+
   type Person {
-			name: String!
-			surname: String!
+    val1: String!
+    val2: String!
+  }
+  type Facility {
+    val3: String!
+    val4: String!
+  }
+  type Exposure {
+    val5: String!
   }
 `;
 
 const resolvers = {
   Query: {
     person: () => {
-      return fetch(`${baseURL}/person`,  { 
-							'Access-Control-Allow-Origin': '*'
-						}).then(res => res.json())
+      return fetch(`${baseURL}/person`, {
+        "Access-Control-Allow-Origin": "*",
+      }).then((res) => res.json());
     },
-    // personById: (args) => {
-    //   const { id } = args
-    //   return fetch(`${baseURL}/person/${id}`).then(res => res.json())
-    // },
-    // exposure: () => {
-    //   return fetch(`${baseURL}/exposure`).then(res => res.json())
-    // },
-    // facility: () => {
-    //   return fetch(`${baseURL}/facility`).then(res => res.json())
-    // },
+    facility: () => {
+      return fetch(`${baseURL}/facility`, {
+        "Access-Control-Allow-Origin": "*",
+      }).then((res) => res.json());
+    },
+    exposure: () => {
+      return fetch(`${baseURL}/exposure`, {
+        "Access-Control-Allow-Origin": "*",
+      }).then((res) => res.json());
+    },
   },
-}
+};
 
-const frontendOrigin = 'http://localhost:3000';
+const frontendOrigin = "http://localhost:3000";
 const corsOptions = {
-	origin: frontendOrigin,
-	credentials: true
+  origin: frontendOrigin,
+  credentials: true,
 };
 
 const app = express();
 
 app.use(cors(corsOptions));
- 
+
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
 });
- 
-server.applyMiddleware({ app, path: '/graphql' });
- 
+
+server.applyMiddleware({ app, path: "/graphql" });
+
 app.listen({ port: 12000 }, () => {
-  console.log('Apollo Server on http://localhost:12000/graphql');
+  console.log("Apollo Server on http://localhost:12000/graphql");
 });
