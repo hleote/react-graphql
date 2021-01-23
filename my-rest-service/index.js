d81355786dfec6f360e2e431678275edb7ac5da7
Hugo <hleote@gmail.com>
2021-01-23 21:54:27 +0100

    Add intended values to REST API

--- my-rest-service/index.js
+++ my-rest-service/index.js
@@ -2,34 +2,30 @@ import express from "express";
 const app = express();
 const port = 11000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.get("/person", function (req, res) {
   const person = {
-    name: "Hugo",
-    surname: "Leote",
+    val1: "",
+    val2: "",
   };
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(person));
 });
 
+app.get("/facility", function (req, res) {
+	const facility = {
+			val3: "",
+			val4: "",
+	};
+	res.setHeader("Content-Type", "application/json");
+	res.end(JSON.stringify(facility));
+});
+
 app.get("/exposure", function (req, res) {
   const exposure = {
-    val5: "test5",
+    val5: "",
   };
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(exposure));
-});
-
-app.get("/facility", function (req, res) {
-  const facility = {
-    val3: "test3",
-    val4: "test4",
-  };
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(facility));
 });
 
 app.listen(port, () => {
