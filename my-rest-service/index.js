1cbcf35281dd8a277f0db61482c0fea1dd12c05b
Hugo <hleote@gmail.com>
2021-01-24 17:42:23 +0100

    REST API logic clean up

--- my-rest-service/index.js
+++ my-rest-service/index.js
@@ -2,28 +2,60 @@ import express from "express";
 const app = express();
 const port = 11000;
 
+// Mocked models
+const person = {
+  val1: null,
+  val2: null,
+};
+const facility = {
+  val3: null,
+  val4: null,
+};
+const exposure = {
+  val5: null,
+};
+
+// --- POST endpoints --------
+
+app.post("/person/:inputValue", function (req, res) {
+  const { inputValue } = req.params;
+  person.val1 = inputValue;
+  person.val2 = inputValue + 1;
+
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(person));
+});
+
+app.post("/facility/:val1", function (req, res) {
+  const { val1 } = req.params;
+  facility.val3 = val1;
+  facility.val4 = val1;
+
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(facility));
+});
+
+app.post("/exposure/:val2", function (req, res) {
+  const { val2 } = req.params;
+  exposure.val5 = val2;
+
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(exposure));
+});
+
+// --- GET endpoints --------
+
 app.get("/person", function (req, res) {
-  const person = {
-    val1: "",
-    val2: "",
-  };
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(person));
 });
 
 app.get("/facility", function (req, res) {
-	const facility = {
-			val3: "",
-			val4: "",
-	};
-	res.setHeader("Content-Type", "application/json");
-	res.end(JSON.stringify(facility));
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(facility));
 });
 
 app.get("/exposure", function (req, res) {
-  const exposure = {
-    val5: "",
-  };
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(exposure));
 });
